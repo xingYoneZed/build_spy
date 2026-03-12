f9e0c8da2958d75e1429d970c8933cb64c1205ce
Всеволодов Михаил Михайлович <MMVsevolodov@uptk766.local>
2026-03-12 16:31:30 +0300

    add ARM GCC wrapper

--- ide_launcher.py
+++ ide_launcher.py
@@ -3,19 +3,15 @@ import subprocess
 
 
 def launch_ide(ide, project_path):
-    if not ide or not ide.get("path"):
-        raise RuntimeError("IDE не выбрана или путь отсутствует")
 
     ide_path = ide["path"]
 
-    if not os.path.isfile(ide_path):
-        raise RuntimeError("Файл IDE не найден: " + ide_path)
-
     env = os.environ.copy()
 
-    # Добавляем папку BuildSpy в PATH
-    buildspy_path = os.path.dirname(os.path.abspath(__file__))
-    env["PATH"] = buildspy_path + os.pathsep + env["PATH"]
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    wrappers_dir = os.path.join(base_dir, "wrappers")
+
+    env["PATH"] = wrappers_dir + os.pathsep + env["PATH"]
 
     subprocess.Popen(
         [ide_path, project_path],
---
+++ wrappers/arm-none-eabi-gcc.bat
@@ -0,0 +1,2 @@
+@echo off
+"%~dp0..\BuildSpy.exe" arm-none-eabi-gcc %*
